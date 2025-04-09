9808ece60cc42f0cf131685c070bcf96f21f1278
caioarchive <caiomiguelxf@gmail.com>
2025-04-09 17:03:26 -0300

    React sessao 1

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,14 @@ const App = () => {
   const nome = "caio"
   const valor1 = 15
   const valor2 = 2
+
+  const h2Styled = {
+    color: "blue",
+    backgroundColor: "black"
+  }
   return (
     <>
-    <h1>Olá mundo, meu nome é {nome}</h1>
+    <h1 className={h2Styled}>Olá mundo, meu nome é {nome}</h1>
     <p>{valor1 *valor2}</p>
   
     </>

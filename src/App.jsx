632d0b0f1d60a7cf96ff3c39e0e28979d1183e8f
caioarchive <caiomiguelxf@gmail.com>
2025-04-09 17:00:37 -0300

    React sessao 1

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+const App = () => {
+  const nome = "caio"
+  const valor1 = 15
+  const valor2 = 2
+  return (
+    <>
+    <h1>Olá mundo, meu nome é {nome}</h1>
+    <p>{valor1 *valor2}</p>
+  
+    </>
+  )
+}
+
+export default App
